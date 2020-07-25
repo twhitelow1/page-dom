e925e14dd842281328d964052d5c679f00746bac
Todd Whitelow <67042489+twhitelow1@users.noreply.github.com>
2020-07-25 03:13:13 -0600

    updaes

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ A SIMPLE PROGRAM MEANT TO DISPLAY THE POWER OF THE DOM AND EVENT HANDLERS.
 const students = document.querySelectorAll('.student-list li');
 const pageDiv = document.querySelector('.page');
 const totalPages = Math.round(students.length / 10);
-
+const studentsDetails = document.querySelectorAll('.student-details h3');
 /******************************************************
 *    
 * START  showPage Function
@@ -110,3 +110,63 @@ showPage(students, 1);
      /******************************************************
    *       END EVENT LISTENER FOR PAGINATION LINKS
    *********************************************************/
+
+   
+   /******************************************************
+   *        
+   *     buildSearchBar Function
+   *
+   *********************************************************/
+   const buildSearchBar = () => {
+      const pageHeader = document.querySelector('.page-header');
+      const div = document.createElement('div');
+      div.classList.add('student-search');
+      const input = document.createElement('input');
+      input.classList.add("search-input");
+      input.setAttribute("placeholder", "Search for students...");
+      const button = document.createElement('button');
+      button.setAttribute("id", "submit");
+      button.textContent = "Search";
+      pageHeader.appendChild(div);
+      div.appendChild(input);
+      div.appendChild(button);
+   }
+
+   buildSearchBar();   
+
+   /* Variables to reference the `input` and search `button` elements */
+   const search = document.querySelector('.search-input');
+   const submit = document.querySelector('#submit');
+   
+   const searchBar = (searchInput, names) => {
+      
+      for(let i = 0; i < names.length; i++) { 
+         const studentName = students[i].children[0].children[1].textContent
+         const name = names[i].textContent.toLowerCase();
+         const searchValue = searchInput.value;
+
+         if(searchInput.value.length !== 0 && name.includes(searchValue.toLowerCase())){
+           names[i].classList.add('match');
+            console.log(studentName);
+         }
+      }
+   };
+
+   /* submit listener */
+submit.addEventListener('click', (event) => {
+   event.preventDefault();
+ 
+   // Invoke your search function here - Arguments: search, tableCells
+  searchBar(search, students);
+ 
+   // Helpful log statement to test function
+   console.log('Submit button is functional!');
+ });
+ 
+ /* submit listener */
+   
+
+
+
+
+  
