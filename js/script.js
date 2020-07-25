40c2fa0337adfe9401c52fa17da6647a3a442485
Todd Whitelow <67042489+twhitelow1@users.noreply.github.com>
2020-07-25 07:29:50 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@ A SIMPLE PROGRAM MEANT TO DISPLAY THE POWER OF THE DOM AND EVENT HANDLERS.
 *********************************************************/
 
 const students = document.querySelectorAll('.student-item');
-
-//const totalPages = Math.ceil(students.length / 10);
+/* Variables to reference the `input` and search `button` elements */
+const search = document.querySelector('.search-input');
+const submit = document.querySelector('#submit');
 const studentsDetails = document.querySelectorAll('.student-details h3');
-let   visibileStudents = students;
+const pagination = document.querySelector('.pagination ul')
 let   itemsPerPage = 10;
 
 
@@ -60,7 +61,7 @@ const appendPageLinks = (list) => {
       return;
    };
 
-   //Build Pagination Div
+   // Build Div for pagination
    const div = document.createElement('div');
    div.className = "pagination";
    pageDiv.appendChild(div);
@@ -84,8 +85,6 @@ const appendPageLinks = (list) => {
    /******************************************************      
    *     EVENT LISTENER FOR PAGINATION LINKS
    *********************************************************/
-
-   const pagination = document.querySelector('.pagination ul')
    div.addEventListener('click', () => {
       // Grab the list of the pagination ul
       pageLinks = pagination.children;    
@@ -94,7 +93,6 @@ const appendPageLinks = (list) => {
       currentPageNum =  parseInt(event.target.textContent); 
       
       /* Refresh Pagination Loop
-      /
       //    Loop through each link 
       //    If the current selectedLink is the same as the current page numnber add active class
       //    Else remove the active class from that links tag.
@@ -128,15 +126,24 @@ const appendPageLinks = (list) => {
    *
    *********************************************************/
 const buildSearchBar = () => {
+      // Grab pageHeader div to place the searchbar
       const pageHeader = document.querySelector('.page-header');
+
+      // Create search div
       const div = document.createElement('div');
       div.classList.add('student-search');
+
+      // Create text input for searchbar
       const input = document.createElement('input');
       input.classList.add("search-input");
       input.setAttribute("placeholder", "Search for students...");
+
+      // Create submit button for searchbar
       const button = document.createElement('button');
       button.setAttribute("id", "submit");
       button.textContent = "Search";
+
+      // Ammend the elments to the page
       pageHeader.appendChild(div);
       div.appendChild(input);
       div.appendChild(button);
@@ -146,11 +153,15 @@ const buildSearchBar = () => {
          event.preventDefault();
          searchBar(input, students);
       });
+
+      /* key listener */
       input.addEventListener('keyup', (event) => {
          event.preventDefault();
          searchBar(input, students);
       });
 }
+
+// Build The Search Bar
 buildSearchBar();
 
 /******************************************************
@@ -158,9 +169,6 @@ buildSearchBar();
 *     searchBar Function
 *
 *********************************************************/
-/* Variables to reference the `input` and search `button` elements */
-const search = document.querySelector('.search-input');
-const submit = document.querySelector('#submit');
 
 const searchBar = (searchInput, names) => {
    // Clear any current pagination
@@ -178,26 +186,31 @@ const searchBar = (searchInput, names) => {
       const name = names[i].textContent.toLowerCase();
       const searchValue = searchInput.value;
 
+      // If input isn't empty and the search input is a substring to any name on list
       if(searchInput.value.length !== 0 && name.includes(searchValue.toLowerCase())){
+         // True -> Set name as a match and display
          names[i].classList.add('match');
          names[i].style.display = 'list-item';
          console.log(studentName);
       } else {
+         // False -> Remove match class and turn off display
          names[i].classList.remove('match');
          names[i].style.display = 'none';
       }
    }
 
+   // If search input value is an empty string then show default page and pagination
    if (searchInput.value.length === 0) {
       showPage(students, 1);
       appendPageLinks(students);
       return;
       };
 
+   // Grab all the student list elements with the class match. Use this group as
+   // the list now instead of students
    const matchItems = document.querySelectorAll('.match');
-      console.log(matchItems);
-      showPage(matchItems, 1);
-      appendPageLinks(matchItems);
+   showPage(matchItems, 1);
+   appendPageLinks(matchItems);
 };
 
  
